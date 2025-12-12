f25286d3e95ec9fa0f4eb0b27b1106f0c4a9c83b
Sebin10 <sebinsanthosh2016@gmail.com>
2025-12-12 07:01:15 +0530

    slight schema changes

--- services/tenant_onboarding.py
+++ services/tenant_onboarding.py
@@ -6,10 +6,11 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-from Models import Tenant, TenantSubscription
+from Models import Tenant
 from Schemas import TenantRequest
 from core.helpers.auth_helper import issue_refresh_token
-from utils.metrics import req_total, req_duration
+from core.user_auth import check_user_authorization
+from utils.metrics import req_total
 from Models import User, UserRole, Role
 from Schemas import LoginRequest, LoginResponse
 from core.db_config import get_db
@@ -25,7 +26,6 @@ async def create_tenant(
         db: Session = Depends(get_db)
 ):
     """Create a new tenant"""
-    start = datetime.now()
     try:
         req_total.labels(operation="create_tenant", status="start").inc()
 
@@ -58,23 +58,10 @@ async def create_tenant(
         db.refresh(user)
 
         # create user role
-        tenant_admin_role_id = db.query(Role.role_id).filter(Role.code == "tenant_admin").first()[0]
+        tenant_admin_role_id = db.query(Role.role_id).filter(Role.code == "tenant-admin").first()[0]
         role = UserRole(id=uuid.uuid4(), tenant_id=tenant.tenant_id, user_id=user.user_id, role_id=tenant_admin_role_id)
         db.add(role)
         db.commit()
-
-        # create a default subscription for the tenant
-        subscription = TenantSubscription(tenant_id=tenant.tenant_id, plan_code="core_01", current_period_start=datetime.now(),
-                                          current_period_end=datetime.now()+timedelta(days=7), is_trial=True)
-        db.add(subscription)
-        db.commit()
-
-        req_total.labels(operation="create_tenant", status="success").inc()
-        req_duration.labels(operation="create_tenant").observe(
-            (datetime.now() - start).total_seconds()
-        )
-
-        logger.info(f"✅ Created tenant: {tenant.tenant_id} ({tenant.tenant_name})")
 
         return {
             "tenant_id": str(tenant.tenant_id),
